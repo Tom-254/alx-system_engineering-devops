1ed8fec9617e9f6ee96586a385080f05bd1fd1f9
Tom-254 <211mseeratiego@gmail.com>
2023-01-16 21:30:39 +0000

    0x15. API

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,41 +1,24 @@
 #!/usr/bin/python3
-"""Script that exports an employee TODO tasks to a json file
-    Is an interger representing an employee id.
+"""
+Script that exports an employee TODO tasks to a json file
+Is an interger representing an employee id.
 """
 import json
 import requests
 import sys
 
 
-def tasks_done(id):
-    """Script that exports an employee TODO tasks to a json file
-        Parameters:
-        employee_id: Is an interger representing an employee id.
-    """
+if __name__ == '__main__':
+    id_c = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                         .format(id_c)).json()
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                         .format(sys.argv[1])).json()
 
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
-    response = requests.get(url)
-    response_json = response.json()
-    employee_name = response_json.get("name")
-
-    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
-    todos = requests.get(url)
-    todos_json = todos.json()
-    task_list = []
-
-    for task in todos_json:
-        task_dict = {}
-        task_dict["task"] = task.get("title")
-        task_dict["completed"] = task.get("completed")
-        task_dict["username"] = employee_name
-        task_list.append(task_dict)
-
-    todos = {"{}".format(id): task_list}
-
-    file_name = "{}.json".format(id)
-    with open(file_name, "a") as fd:
-        json.dump(todos, fd)
-
-
-if __name__ == "__main__":
-    tasks_done(sys.argv[1])
+    with open("{}.json".format(id_c), "w") as user_id:
+        json.dump({id_c: [{
+            'task': task.get('title'),
+            'completed': task.get('completed'),
+            'username': users.get('username')
+        } for task in todos]}, user_id)
